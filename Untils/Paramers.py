af248ebb06859683951c6c3d07afef66a15e9ad3
mi4da <55870943+mi4da@users.noreply.github.com>
2021-08-03 17:27:43 +0800

    Update Paramers.py

--- Untils/Paramers.py
+++ Untils/Paramers.py
@@ -1,12 +1,18 @@
 """
 Transformer模型参数
 """
+# 编解码器的个数
 num_layers = 6
+# 词向量的维数
 d_model = 512
+# 隐藏神经元个数
 dff = 2048
+# n头注意力
 num_heads = 8
 # input_vocab_size = subword_encoder_zh.vocab_size + 2
+# 输入词表大小
 input_vocab_size = 2 ** 13
+# 目标词表大小
 # target_vocab_size = subword_encoder_en.vocab_size + 2
 target_vocab_size = 2 ** 13
 dropout_rate = 0.1
